0144f5580af4f768437e12593b7814890df5c1ee
Dustin Boswell <dustinboswell@server>
2012-07-03 01:07:41 -0700

    1st

---
+++ classifier.py
@@ -0,0 +1,98 @@
+from stats import SummaryStats, Histogram
+
+from training_examples import TrainingExamples
+
+class BinaryClassifier:
+    """Abstract base class for any classifier that is trained on examples whose
+    outputs are either 0 or 1.  Also provides a test() method."""
+
+    def prob_output1(self, example):
+        """What is the probability that this example has an _OUTPUT of 1?"""
+        raise "NotImplemented"
+
+    def train(self, training_examples):
+        raise "NotImplemented"
+
+    def test(self, training_examples):
+        """Computes the "area under the ROC curve". This is a way to measure the
+        precision/recall WITHOUT choosing a cutoff-threshold.  It is mathematically
+        equivalent to:
+           "the probability that a random positive example has a higher
+            prob_output1 than a random negative case"
+        (This equivalence is non-obvious).
+
+        The algorithm below computes this average probability by effectively trying
+        all combinations of positive-vs-negative examples, but does this in O(NlgN)
+        instead of O(N^2)"""
+        if type(training_examples) is TrainingExamples:
+            training_examples = training_examples.examples
+
+        prob_stats = SummaryStats()
+        prob_hist = Histogram()
+        output1_scores = list()
+        output0_scores = list()
+        for example in training_examples:
+            prob = self.prob_output1(example)
+            prob_stats.add(prob)
+            prob_key = "%1.1f-%1.1f" % (int(prob*10)/10.0, (int(prob*10)+1)/10.0)
+            if prob == 1: prob_key = "0.9-1.0"  # don't create a 1.0-1.1 bucket
+            prob_hist.add(prob_key)
+            real_output = (example["_OUTPUT"] == 1)
+            if real_output:
+                output1_scores.append(prob)
+            else:
+                output0_scores.append(prob)
+
+        output1_scores.sort()
+        output0_scores.sort()
+
+        # TODO: make debug_output a new function param
+        if False:
+            print "%d output1 scores:" % len(output1_scores),
+            print ["%2.2f" % i for i in output1_scores[0:5]],
+            print " ... ",
+            print ["%2.2f" % i for i in output1_scores[-5:]]
+
+            print "%d output0 scores:" % len(output0_scores),
+            print ["%2.2f" % i for i in output0_scores[0:5]],
+            print " ... ",
+            print ["%2.2f" % i for i in output0_scores[-5:]]
+
+        j = 0
+        num_correct_rank = 0
+        for i in xrange(len(output1_scores)):
+            o1_score = output1_scores[i]
+            while j < len(output0_scores) and output0_scores[j] < o1_score: j += 1
+            num_correct_rank += j
+        rank_accuracy = float(num_correct_rank) / (len(output0_scores)*len(output1_scores))
+        print "Ranking accuracy: ('area under the ROC curve') %2.3f" % rank_accuracy
+        print "Prob output: mean=%1.3f std=%1.3f" % (prob_stats.avg(), prob_stats.std())
+        print "Histogram of prob output:", prob_hist
+        return rank_accuracy
+
+import random
+class RandomBinaryClassifier(BinaryClassifier):
+    def prob_output1(self, example):
+        return random.random()
+
+if __name__ == "__main__":
+    # data set where:  x > 0 <==> output is 1
+    te = TrainingExamples()
+    for x in xrange(1, 1000):
+        te.add_example({ 'x': x}, 1)
+        te.add_example({ 'x': -x}, 0)
+
+    # A random classifier should have 50% area under the curve.
+    random_binary_classifier = RandomBinaryClassifier()
+    area_under_curve = random_binary_classifier.test(te)
+    assert 0.45 <= area_under_curve <= 0.55
+
+    # A perfectly correct classifier should have 100% area under curve.
+    class PerfectBinaryClassifier(BinaryClassifier):
+        def prob_output1(self, example):
+            if example['x'] > 0: return 1
+            else: return 0
+
+    perfect_binary_classifier = PerfectBinaryClassifier()
+    area_under_curve = perfect_binary_classifier.test(te)
+    assert 0.99 <= area_under_curve <= 1.01
---
+++ training_examples.py
@@ -0,0 +1,140 @@
+import collections
+import json
+import cPickle
+
+class TrainingExamples:
+    """Used to store a set of input/output "training" examples for supervised learning.
+    Each example's input is a dictionary of feature_name => feature_value,
+    where feature_name is a string, and feature_value is a float/int.
+    Each output value is also a float/int."""
+
+    def __init__(self):
+        self.examples = []
+        self.feature_names = set()  # union of all feature names in examples
+        self.counts = collections.defaultdict(int)
+
+    def add_example(self, features, output_value):
+        """Add the example to our list, and update various counts about it."""
+        self.counts[self._key('*', '*', output_value)] += 1
+
+        for (name, value) in features.iteritems():
+            if name.startswith("_"): continue
+            self.feature_names.add(name)
+
+            self.counts[self._key(name, '*', '*')] += 1
+            self.counts[self._key(name, '*', output_value)] += 1
+            self.counts[self._key(name, value, '*')] += 1
+            self.counts[self._key(name, value, output_value)] += 1
+
+        features["_OUTPUT"] = output_value  # sneak into the dict
+        self.examples.append(features)
+
+    def count(self, feature_name='*', feature_value='*', output_value='*'):
+        """Returns how many of our examples match the given criteria.
+        The default of '*' means 'matches any'."""
+        key = self._key(feature_name, feature_value, output_value)
+        return self.counts.get(key, 0)
+
+    @staticmethod
+    def _key(feature_name, feature_value, output_value):
+        return "%s--%s--%s" % (feature_name, feature_value, output_value)
+
+    def normalize_one_example(self, example):
+        """Normalize the feature values of this example to the range [0, 1], which is
+        the percentile of that feature values, given the distribution seen in the training
+        set.  Note that @example doesn't need to be in the training set.
+
+        Assumes normalize_feature_values() has been called."""
+        if example.get("_NORMALIZED", False): return  # already normalized
+        for (name, value) in example.items():
+            if name.startswith("_"):
+                continue
+
+            # get all the (sorted) values we've seen for this feature name.
+            values = self.unnormalized_feature_values[name]
+
+            if value <= values[0]:  # smaller than anything we've ever seen
+                norm_value = 0.0
+            elif value >= values[-1]:  # larger than anything we've ever seen
+                norm_value = 1.0
+            else:
+                for quant_i in [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]:
+                    quant_value = values[int(quant_i*len(values))]
+                    if value >= quant_value:
+                        norm_value = quant_i
+                        break
+
+            # replace the feature value with the normalized value
+            example[name] = norm_value
+            example["_ORIG_" + name] = value  # save the old value
+
+        example["_NORMALIZED"] = True
+
+    def normalize_feature_values(self):
+        """1) Calculate the feature value distribution for each feature (over all
+        example's values)
+        2) Replace each feature value with a value in [0,1] which represents it's
+        "percentile" -- 0.0 means the value is amoung the lowest seen."""
+        num_already_normalized = 0
+        self.unnormalized_feature_values = {}  # name -> [values]
+        for ex in self.examples:
+            if ex.get("_NORMALIZED", False):
+                num_already_normalized += 1
+            for (name, value) in ex.iteritems():
+                if name.startswith("_"): continue
+                self.unnormalized_feature_values.setdefault(name, []).append(value)
+        for array in self.unnormalized_feature_values.itervalues():
+            array.sort()
+
+        if num_already_normalized:  # can't mix/match normalize/unnormalized
+            assert num_already_normalized == len(self.examples)
+
+        for ex in self.examples:
+            self.normalize_one_example(ex)
+
+    def save_to_file(self, filename, num_examples=999999999):
+        with open(filename, 'w') as fout:
+            # the entire output is a json list of dictionaries.
+            fout.write(json.dumps(list(self.examples[0:num_examples])))
+
+    def load_from_file(self, filename):
+        examples = json.loads(open(filename).read())
+        for ex in examples:
+            # probably not necessary to remote '_OUTPUT', but add_example() will add it
+            output = ex["_OUTPUT"]
+            del ex["_OUTPUT"]
+            self.add_example(ex, output)
+
+    def pickle_load(self, filename):
+        f = open(filename,'rb')
+        tmp_dict = cPickle.load(f)
+        f.close()
+        self.__dict__.update(tmp_dict)
+
+    def pickle_save(self, filename):
+        f = open(filename,'wb')
+        cPickle.dump(self.__dict__, f, 2)
+        f.close()
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return self.examples[i]
+
+# Example usage.
+if __name__ == "__main__":
+    te = TrainingExamples()
+    te.add_example({ 'fat': 300}, 1)
+    te.add_example({ 'fat': 300}, 1)
+    te.add_example({ 'fat': 100}, 0)
+
+    for example in te:
+        print example
+    te.pickle_save("/tmp/training_examples.tmp")
+    te.pickle_load("/tmp/training_examples.tmp")
+
+    for example in te:
+        print example
+    te.pickle_save("/tmp/training_examples2.tmp")
+
